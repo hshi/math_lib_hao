c6b7b1cd008e7e97dda2a85bca40a8b02b60a1d3
boruoshihao <boruoshihao@gmail.com>
2015-04-17 11:51:34 -0400

    Update coshx_eq_expy, make the return value pure real or pure imaginary.

--- src/math_container.cpp
+++ src/math_container.cpp
@@ -8,5 +8,11 @@ using std::complex;
 complex<double> coshx_eq_expy(double y)
 {
     complex<double> ey={exp(y),0};
-    return log(ey-sqrt(ey*ey-1.0));
+    complex<double> gamma=log(ey-sqrt(ey*ey-1.0));
+
+    //Since gamma is pure real or pure imaginary, set it:
+    if( abs( gamma.real() ) < abs( gamma.imag() ) )  gamma=complex<double>( 0, gamma.imag() );
+    else                                             gamma=complex<double>( gamma.real(), 0 );
+
+    return gamma;
 }
