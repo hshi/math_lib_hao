e71fae69ffdf51ba00152a164bc978279f9594e3
boruoshihao <boruoshihao@gmail.com>
2015-10-21 21:10:52 -0400

    Add one test (example) to use a lamba.

--- test/brents_fun_test.cpp
+++ test/brents_fun_test.cpp
@@ -11,6 +11,12 @@ double fun2(double x)
 {
   return x*x-2.0*x-3;
 }
+
+double fun3(double x,double y)
+{
+  return x*x-2.0*x-1.0-y;
+}
+
 void brents_fun_test()
 {
      double eta=1e-8;
@@ -23,4 +29,7 @@ void brents_fun_test()
      if(abs(x-3.0)<eta) cout<<"brents_fun passed quadratic order test!\n";
      else cout<<"Warning!!!!brents_fun failed quadratic order test!!\n";
 
+     x=brents_fun([](double x) { return fun3(x, 7.0); },0.0,5.0,eta);
+     if(abs(x-4.0)<eta) cout<<"brents_fun passed partially function quadratic order test!\n";
+     else cout<<"Warning!!!!brents_fun failed partially function quadratic order test!!\n";
 }
